868625ca0f7eebd96e2cb5927643a5c3be874ad5
David Kruse <thedavidkruse@gmail.com>
2024-03-10 18:25:02 -0700

    initial commit

---
+++ main.py
@@ -0,0 +1,58 @@
+import csv
+def main():
+    # look at total of sales hstory report of all sales together
+    csv.open('sales_history_report.csv')
+        for column in columns
+        saleNumber += sale
+    # compare to the total of the royalty basis amount on the royalty report
+    csv.open('royalty_report.csv')
+        for column in columns
+        royaltyNumber += sale
+    
+    if saleNumber != royaltyNumber:
+    # concat invoice number and invoice line number on sales history report
+        cc = shr[invoice_number] + shr[line_number]
+        rr[invoice_and_line_number[cc]]
+    # if not equal
+        
+    # pivot table of concat invoice+lineNumber from the sales history report and compare it to pivot table of invoice and line number of royalty report
+    
+    # compare the sales value from sales history to the royalty basis amount on the royalty report
+    
+    # if values != that is > 0.05$ or < -0.05$ then inboice and line number is missing from royalty report
+    
+    # figure out why
+    # 
+        
+
+
+    # Possible failure points:
+        # Not both csv's
+        # if invoice number and invoice line number concatenation doesn't exist on the shr
+        # values are not within +/- 0.05$
+    
+# possible time save 1 hrs
+        
+        ########################################################
+        ############## STEP 2 #################################
+        ########################################################
+
+# put formula in pivot table to tell us whether the id or invioce or line number exists on the royalty report
+# if no
+# look up the invoice number from the rr take the infgormation except for the invoice line number, invoice prodyuct code, ... put it at the bottom of the rr
+# remove whatever column values from that line and replace values with appropriate invoice and line number values
+# look up invoice product code and copy the values from that line to the new line we created
+# take sales value from the shr that correlates to the invoice and line number and put them into the new line we created
+
+# possible time save 3 hr
+
+        ########################################################
+        ############## STEP 3 #################################
+        ########################################################
+
+# difference between the invoice and line number in the shr and the royalty report is > +/- 0.05$
+# research royalty class for contractual problem
+        
+        ########################################################
+        ############## STEP 4 #################################
+        ########################################################
